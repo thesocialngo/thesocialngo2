d6009f180461eb6349cd4f6006edbade24f1d89e
Krishna Kotha <krishna_kotha@cable.comcast.com>
2017-01-07 15:52:20 -0500

    First cut commit

---
+++ src/test/java/com/thesocialngo/ThesocialngoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thesocialngo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThesocialngoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
